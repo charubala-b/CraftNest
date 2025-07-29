3a9f8cf4180778a0505089537947e69220fc7250
charubala-b <charubala.b2022cse@sece.ac.in>
2025-07-29 15:28:57 +0530

    added mailcatcher

--- db/migrate/20250625052710_remove_devise_fields_from_users.rb
+++ db/migrate/20250625052710_remove_devise_fields_from_users.rb
@@ -3,6 +3,5 @@ class RemoveDeviseFieldsFromUsers < ActiveRecord::Migration[7.2]
     remove_column :users, :reset_password_token, :string
     remove_column :users, :reset_password_sent_at, :datetime
     remove_column :users, :remember_created_at, :datetime
-    # Add more if needed (confirmable, lockable, etc.)
   end
 end
