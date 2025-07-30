b153dd7f7b1919491f99d0a63f9de726c61ce62c
charubala-b <charubala.b2022cse@sece.ac.in>
2025-07-30 11:03:10 +0530

    solved some errors

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,13 +6,10 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "@rails/ujs", to: "rails-ujs.js"
 
-pin "enums", to: "enums.js"
 pin_all_from "app/javascript/modifiers", under: "modifiers"
 
 pin "chartkick", to: "chartkick.js"
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js"
 pin "chartjs-adapter-date-fns", to: "https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js"
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
--- spec/factories/doorkeeper_access_tokens.rb
+++ spec/factories/doorkeeper_access_tokens.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :doorkeeper_access_token, class: 'Doorkeeper::AccessToken' do
     application { create(:oauth_application) }
     resource_owner_id { create(:user).id }
-    scopes { 'read write' }         # Use valid scope to pass scope validation
-    expires_in { 2.hours.to_i }     # Use integer format in seconds
-    revoked_at { nil }              # Ensures token is active
+    scopes { 'read write' }    
+    expires_in { 2.hours.to_i }
+    revoked_at { nil }           
   end
 end
